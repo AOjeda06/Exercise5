0bbed2a089c8ef4ebfbcc11410b9bda6d03fa1db
andres.ojeda <andres.ojeda@106-24.info2>
2024-09-20 14:13:32 +0200

    Exercise5

---
+++ Exercise5/src/exercise5/Exercise5.java
@@ -0,0 +1,12 @@
+package exercise5;
+
+public class Exercise5 {
+
+	public static void main(String[] args) {
+		System.out.println("Andrés Ojeda Rodríguez ");
+		System.out.println("C/ Mataleon de Carvajal 13");
+		System.out.println();
+		System.out.println("+34 666 777 888");
+	}
+
+}
